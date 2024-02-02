53529087af332e5f241e52407082a39733e64515
Axel C. Lopez <lopezaxel@protonmail.com>
2024-02-02 16:01:51 -0300

    Script works for polis P. Patricios and M. Fierro

--- main.py
+++ main.py
@@ -6,105 +6,139 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from polideportivos_data import polis
 
 fake = Faker(["es_AR"])
 
-polis = [
-    { name: "Parque Patricios", canchas: [5, 7], url:"" },
-    { name: "Costa Rica", canchas: [5], url:"https://buenosaires.gob.ar/desarrolloeconomico/deportes/actividades/futbol-5-en-el-polideportivo-costa-rica" },
-    { name: "Santojanni", canchas: [5, 9], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-santojanni" },
-    { name: "Avellaneda", canchas: [7, 9, 11], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-avellaneda" },
-    { name: "Colegiales", canchas: [5], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-colegiales" },
-    { name: "Don Pepe", canchas: [5], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-don-pepe" },
-    { name: "Dorrego", canchas: [5], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-dorrego" },
-    { name: "Martin Fierro", canchas: [5, 7], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-martin-fierro" },
-    { name: "Onega", canchas: [5], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-onega" },
-    { name: "Pereyra", canchas: [5, 7], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-pereyra" },
-    { name: "Pomar", canchas: [9], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-pomar" },
-    { name: "Sarmiento", canchas: [5], url:"https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-sarmiento" },
-]
+supported_browsers = [{"name": "Firefox"}, {"name": "Chrome"}]
 
-supported_browsers = [
-    { name: "Firefox" }, { name: "chrome" }
-]
+SPEED = 2
+
 
 def get_person_trait(trait):
-    if (trait == "first_name"):
+    if trait == "first_name":
         return fake.first_name()
-    elif (trait == "last_name"):
+    elif trait == "last_name":
         return fake.last_name()
-    elif (trait == "email"):
+    elif trait == "email":
         return fake.ascii_free_email()
+
 
 def get_random_dni():
     return randint(10000000, 47000000)
 
+
 def get_poli_url():
-    for i in enumerate(polis):
-        print(f"{i} - Polideportivo {polis[i].name}")
-    poli_idx = input("Ingrese el numero del polideportivo deseado: ")
-    return polis[poli_idx]
+    for i, poli in enumerate(polis):
+        print(f"{i} - Polideportivo {polis[i]['name']}")
+    return int(input("Ingrese el numero del polideportivo deseado: "))
+
 
 def get_cancha_size(poli_idx):
-    canchas = polis[poli_idx].canchas
-    for i in enumerate(canchas):
-        print("f{i} - Cancha futbol {canchas[i]}")
-    cancha_idx = input("Ingrese el numero de la cancha deseada: ")
+    canchas = polis[poli_idx]["canchas"]
+    for i, poli in enumerate(canchas):
+        print(f"{i} - Cancha futbol {canchas[i]}")
+    cancha_idx = int(input("Ingrese el numero de la cancha deseada: "))
     return canchas[cancha_idx]
 
+
 def get_browser_name():
-    for i in enumerate(supported_browsers):
-        print(f"{i} - {supported_browsers[i].name}")
-    browser_idx = input("Ingrese el numero del navegador web a utilizar: ")
-    return supported_browsers[browser_idx].idx;
+    for i, poli in enumerate(supported_browsers):
+        print(f"{i} - {supported_browsers[i]['name']}")
+    browser_idx = int(input("Ingrese el numero del navegador web a utilizar: "))
+    return supported_browsers[browser_idx]["name"]
+
 
 def get_browser():
     browser_name = get_browser_name()
-    if browser_name == "firefox":
+    if browser_name == "Firefox":
         return webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-    elif browser_name == "chrome":
+    elif browser_name == "Chrome":
         return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+
 def main():
-    [poli_url, poli_idx] = get_poli_url()
+    poli_idx = get_poli_url()
     cancha_day = input("Ingresar dia de reserva de la cancha: ")
     cancha_hour = input("Ingresar la hora de reserva de la cancha: ")
     cancha_size = get_cancha_size(poli_idx)
-    cancha_type = input("Ingresar el tipo de cancha, elegir asfalto o sintetico: ")
+    if polis[poli_idx]["name"] == "Parque Patricios":
+        cancha_type = input("Ingresar el tipo de cancha, elegir asfalto o sintetico: ")
+    elif polis[poli_idx]["name"] == "Martin Fierro":
+        cancha_type = input("Ingresar numero de cancha, 1 o 2: ")
     user_email = input("Ingresar el email de tu cuenta miBA: ")
     user_password = input("Ingresar la contraseña de tu cuenta miBA: ")
-
     driver = get_browser()
-    driver.get(poli_url)
-    # https://formulario-sigeci.buenosaires.gob.ar/InicioTramite?idPrestacion=
-    driver.find_element(By.PARTIAL_LINK_TEXT, "https://formulario-sigeci.buenosaires.gob.ar/InicioTramite?idPrestacion=").click()
-    sleep(10)
+
+    # Entrar a la pagina de la cancha
+    driver.get(polis[poli_idx]["url"])
+
+    # Si tiene una sola cancha clickearla, si son mas elegirla
+    if len(polis[poli_idx]["canchas"]) == 1:
+        driver.find_element(
+            By.PARTIAL_LINK_TEXT,
+            "https://formulario-sigeci.buenosaires.gob.ar/InicioTramite?idPrestacion=",
+        ).click()
+    else:
+        # t = f"//a[contains(text(), 'fútbol {cancha_size}')]"
+        driver.find_element(
+            By.XPATH, f"//a[contains(text(), 'fútbol {cancha_size}')]"
+        ).click()
+    sleep(SPEED)
+
+    # Loguearse con email
     driver.find_element(By.ID, "zocial-mail").click()
-    sleep(4)
+    sleep(SPEED)
+
+    # Ingresar email, contrasena y dar click en el button de login
     driver.find_element(By.ID, "email").send_keys(user_email)
     driver.find_element(By.ID, "password-text-field").send_keys(user_password)
     driver.find_element(By.ID, "login").click()
-    sleep(4)
-    # driver.find_element(By.XPATH, f"//div[@data-start]='fecha'").click()
-    driver.find_element(By.XPATH, f"//h4[@data-start]='2024-02-{cancha_day}'").click()
-    sleep(4)
-    driver.find_element(By.XPATH, f"//input[@data-start]='2024-02-{cancha_day}'").click()
-    sleep(4)
-    # driver.find_element(By.XPATH, f"//input[text()={cancha_type}]").click();
-    # sleep(4)
-    driver.find_element(By.ID, "save").click()
-    sleep(4)
-    driver.find_element(By.XPATH, f"//input[@data-hour]='{cancha_hour}:00 hs.'").click()
-    sleep(4)
-    driver.find_element(By.ID, "next").click()
-    sleep(4)
-    nombre_inputs = driver.find_elements(By.XPATH, "//input[contains(@id, 'form_Nombre')]");
-    apellido_inputs = driver.find_elements(By.XPATH, "//input[contains(@id, 'form_Apellido')]");
-    email_inputs = driver.find_elements(By.XPATH, "//input[contains(@id, 'form_Email')]");
-    # strange email with different id form_emailA5REQ
-    email_inputs.append(driver.find_element(By.XPATH, "//input[contains(@id, 'form_email')]"))
-    dni_inputs = driver.find_elements(By.XPATH, "//input[contains(@id, 'form_TipoyNroDocA')][@type='number']");
+    sleep(SPEED)
 
+    #     driver.find_element(By.XPATH, "//input[contains(@id, 'form_email')]")
+    # Clickear en el link de elegir cancha for fecha
+    driver.find_element(By.XPATH, "//div[@data-start='fecha']").click()
+    sleep(SPEED)
+
+    # Elegir dia de la cancha
+    driver.find_element(By.XPATH, f"//h4[@data-date='2024-02-{cancha_day}']").click()
+    sleep(SPEED)
+
+    # Elegir cancha dependiendo del polideportivo
+    driver.find_element(
+        By.XPATH, f"//input[contains(@data-placename, '{cancha_type}')]"
+    ).click()
+    sleep(SPEED)
+
+    # Clickear en el boton de guardar y continuar
+    driver.find_element(By.ID, "save").click()
+    sleep(SPEED)
+
+    # Elegir hora de la cancha
+    driver.find_element(By.XPATH, f"//input[@data-hour='{cancha_hour}:00']").click()
+    sleep(SPEED)
+
+    # Clickear en el boton de siguiente
+    driver.find_element(By.ID, "next").click()
+    sleep(SPEED)
+
+    # Elegir todos los inputs de nombre, apellido, email y dni
+    nombre_inputs = driver.find_elements(
+        By.XPATH, "//input[contains(@id, 'form_Nombre')]"
+    )
+    apellido_inputs = driver.find_elements(
+        By.XPATH, "//input[contains(@id, 'form_Apellido')]"
+    )
+    email_inputs = driver.find_elements(
+        By.XPATH, "//input[contains(@id, 'form_Email')]"
+    )
+    dni_inputs = driver.find_elements(
+        By.XPATH, "//input[contains(@id, 'form_TipoyNroDocA')][@type='number']"
+    )
+    sleep(SPEED)
+
+    # Rellenar todos los inputs de nombre, apellido, email y dni con datos falsos de Faker
     for nombre_input in nombre_inputs:
         nombre_input.send_keys(get_person_trait("first_name"))
     for apellido_input in apellido_inputs:
@@ -114,10 +148,17 @@ def main():
     for dni_input in dni_inputs:
         dni_input.send_keys(get_random_dni())
 
-    driver.find_element(By.ID, "exit").click()
+    # Clickear en el boton de siguiente
+    driver.find_element(By.XPATH, "//button[contains(text(), 'Siguiente')]").click()
+    sleep(SPEED)
+
+    # Clickear en el boton de confirmar turno
     driver.find_element(By.ID, "confirmarTurno").click()
-    sleep(4)
-    driver.find_element(By.CLASS_NAME, "siguiente").click()
+    sleep(SPEED)
+
+    # Clickear en el boton de siguiente
+    driver.find_element(By.XPATH, "//button[contains(text(), 'Finalizar')]").click()
+
 
 if __name__ == "__main__":
     main()
---
+++ polideportivos_data.py
@@ -0,0 +1,63 @@
+polis = [
+    {
+        "name": "Parque Patricios",
+        "canchas": [5, 7],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-parque-patricios",
+        "tipo": ["Asfalto", "Sintético"],
+    },
+    {
+        "name": "Costa Rica",
+        "canchas": [5],
+        "url": "https://buenosaires.gob.ar/desarrolloeconomico/deportes/actividades/futbol-5-en-el-polideportivo-costa-rica",
+    },
+    {
+        "name": "Santojanni",
+        "canchas": [5, 9],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-santojanni",
+    },
+    {
+        "name": "Avellaneda",
+        "canchas": [7, 9, 11],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-avellaneda",
+    },
+    {
+        "name": "Colegiales",
+        "canchas": [5],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-colegiales",
+    },
+    {
+        "name": "Don Pepe",
+        "canchas": [5],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-don-pepe",
+    },
+    {
+        "name": "Dorrego",
+        "canchas": [5],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-dorrego",
+    },
+    {
+        "name": "Martin Fierro",
+        "canchas": [5, 7],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-martin-fierro",
+    },
+    {
+        "name": "Onega",
+        "canchas": [5],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-onega",
+    },
+    {
+        "name": "Pereyra",
+        "canchas": [5, 7],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-pereyra",
+    },
+    {
+        "name": "Pomar",
+        "canchas": [9],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-pomar",
+    },
+    {
+        "name": "Sarmiento",
+        "canchas": [5],
+        "url": "https://buenosaires.gob.ar/jefaturadegabinete/deportes/futbol-en-el-polideportivo-sarmiento",
+    },
+]
