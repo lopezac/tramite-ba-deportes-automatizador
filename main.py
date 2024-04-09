d1c183da9d756774549d33988a7c152c391e093c
Axel C. Lopez <lopezaxel@protonmail.com>
2024-04-09 20:39:50 -0300

    Improve code,  dont hardcode month and year

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
+from time import sleep, strftime
 from polideportivos_data import polis
 
 fake = Faker(["es_AR"])
@@ -15,13 +15,17 @@ supported_browsers = [{"name": "Firefox"}, {"name": "Chrome"}]
 SPEED = 2
 
 
-def get_person_trait(trait):
-    if trait == "first_name":
-        return fake.first_name()
-    elif trait == "last_name":
-        return fake.last_name()
-    elif trait == "email":
-        return fake.ascii_free_email()
+def get_person_trait(trait_name):
+    trait = ""
+
+    if trait_name == "first_name":
+        trait = fake.first_name()
+    elif trait_name == "last_name":
+        trait = fake.last_name()
+    elif trait_name == "email":
+        trait = fake.ascii_free_email()
+
+    return trait
 
 
 def get_random_dni():
@@ -29,32 +33,54 @@ def get_random_dni():
 
 
 def get_poli_url():
-    for i, poli in enumerate(polis):
-        print(f"{i} - Polideportivo {polis[i]['name']}")
+    for idx in range(len(polis)):
+        print(f"{idx} - Polideportivo {polis[idx]["name"]}")
+
     return int(input("Ingrese el numero del polideportivo deseado: "))
 
 
 def get_cancha_size(poli_idx):
     canchas = polis[poli_idx]["canchas"]
-    for i, poli in enumerate(canchas):
-        print(f"{i} - Cancha futbol {canchas[i]}")
+
+    for idx in range(len(canchas)):
+        print(f"{idx} - Cancha futbol {canchas[idx]}")
     cancha_idx = int(input("Ingrese el numero de la cancha deseada: "))
+
     return canchas[cancha_idx]
 
+def get_cancha_type(poli_idx):
+    poli_name = polis[poli_idx]["name"]
+    cancha_type = ""
+    
+    if poli_name == "Parque Patricios":
+        print("0 - Asfalto")
+        print("1 - Sintetico")
+        cancha_type = int(input("Ingresar el tipo de cancha: "))
+    elif poli_name == "Martin Fierro":
+        print("0 - Cancha 1")
+        print("1 - Cancha 2")
+        cancha_type = int(input("Ingresar el numero de cancha: "))
+
+    return cancha_type
 
 def get_browser_name():
-    for i, poli in enumerate(supported_browsers):
-        print(f"{i} - {supported_browsers[i]['name']}")
+    for idx in range(len(supported_browsers)):
+        print(f"{idx} - {supported_browsers[idx]['name']}")
     browser_idx = int(input("Ingrese el numero del navegador web a utilizar: "))
+
     return supported_browsers[browser_idx]["name"]
 
 
 def get_browser():
     browser_name = get_browser_name()
+    browser = ""
+
     if browser_name == "Firefox":
-        return webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
     elif browser_name == "Chrome":
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    return browser
 
 
 def main():
@@ -62,10 +88,7 @@ def main():
     cancha_day = input("Ingresar dia de reserva de la cancha: ")
     cancha_hour = input("Ingresar la hora de reserva de la cancha: ")
     cancha_size = get_cancha_size(poli_idx)
-    if polis[poli_idx]["name"] == "Parque Patricios":
-        cancha_type = input("Ingresar el tipo de cancha, elegir asfalto o sintetico: ")
-    elif polis[poli_idx]["name"] == "Martin Fierro":
-        cancha_type = input("Ingresar numero de cancha, 1 o 2: ")
+    cancha_type = get_cancha_type(poli_idx)
     user_email = input("Ingresar el email de tu cuenta miBA: ")
     user_password = input("Ingresar la contraseña de tu cuenta miBA: ")
     driver = get_browser()
@@ -102,14 +125,17 @@ def main():
     sleep(SPEED)
 
     # Elegir dia de la cancha
-    driver.find_element(By.XPATH, f"//h4[@data-date='2024-02-{cancha_day}']").click()
+    month = strftime("%m")
+    year = strftime("%Y")
+    driver.find_element(By.XPATH, f"//h4[@data-date='{year}-{month}-{cancha_day}']").click()
     sleep(SPEED)
 
     # Elegir cancha dependiendo del polideportivo
-    driver.find_element(
-        By.XPATH, f"//input[contains(@data-placename, '{cancha_type}')]"
-    ).click()
-    sleep(SPEED)
+    if cancha_type:
+        driver.find_element(
+            By.XPATH, f"//input[contains(@data-placename, '{cancha_type}')]"
+        ).click()
+        sleep(SPEED)
 
     # Clickear en el boton de guardar y continuar
     driver.find_element(By.ID, "save").click()
@@ -141,10 +167,13 @@ def main():
     # Rellenar todos los inputs de nombre, apellido, email y dni con datos falsos de Faker
     for nombre_input in nombre_inputs:
         nombre_input.send_keys(get_person_trait("first_name"))
+
     for apellido_input in apellido_inputs:
         apellido_input.send_keys(get_person_trait("last_name"))
+
     for email_input in email_inputs:
         email_input.send_keys(get_person_trait("email"))
+
     for dni_input in dni_inputs:
         dni_input.send_keys(get_random_dni())
 
@@ -158,6 +187,8 @@ def main():
 
     # Clickear en el boton de siguiente
     driver.find_element(By.XPATH, "//button[contains(text(), 'Finalizar')]").click()
+
+    # close browser and informar que fue un exito la reserva
 
 
 if __name__ == "__main__":
